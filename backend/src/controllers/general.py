08175996583fa54bc482a0c4ab66047a7bdb1eec
Yaakov Neuman <yakinew@yahoo.com>
2025-07-15 13:28:42 +0300

    feat(backend) : Redirect the path "/" to "/ui/"
    Issue: #32
    
    Signed-off-by: Yaakov Neuman <yakinew@yahoo.com>

--- backend/src/controllers/general.py
+++ backend/src/controllers/general.py
@@ -1,3 +1,13 @@
+from flask import redirect, Blueprint
+
+redirect_blueprint = Blueprint('redirect', __name__)
+
+
+@redirect_blueprint.route('/')
+def root_redirect():
+    return redirect('/ui/')
+
+
 def health_status():
     """Get health status."""
     return {
